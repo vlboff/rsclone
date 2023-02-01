2b22427823c0aea1241180da34bbb7d48d6e0c16
Uladzimir <vl.barbikov@gmail.com>
2023-02-01 22:35:54 +0300

    feat: add card active mode

--- src/components/Mix.tsx
+++ src/components/Mix.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { PlayCardIcon } from "../icons";
 
 interface IMix {
   name: string;
 }
 
 function Mix({ name }: IMix) {
+  const [activeCardMode, setActiveCardMode] = useState("");
+
   return (
-    <div className="card">
+    <div
+      className={`card ${activeCardMode}`}
+      onMouseEnter={() => setActiveCardMode("hover")}
+      onMouseLeave={() => setActiveCardMode("")}
+    >
       <div className="card-img"></div>
       <div className="card-name">{name}</div>
       <div className="card-dscr">
@@ -14,6 +21,9 @@ function Mix({ name }: IMix) {
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation et dolore magna aliqua. Ut enim ad
         minim veniam, quis nostrud exercitation
+      </div>
+      <div className={`card-play-btn ${activeCardMode}`}>
+        <PlayCardIcon />
       </div>
     </div>
   );
--- src/icons.ts
+++ src/icons.ts
@@ -5,7 +5,7 @@ import { ReactComponent as SearchIcon } from "./assets/search-icon.svg";
 import { ReactComponent as SearchIconActive } from "./assets/search-icon-active.svg";
 import { ReactComponent as AddPlayListIcon } from "./assets/add-playlist-icon.svg";
 import { ReactComponent as SettingsIcon } from "./assets/settings-icon.svg";
-import { ReactComponent as PlayIcon } from "./assets/settings-icon.svg";
+import { ReactComponent as PlayCardIcon } from "./assets/playCard-icon.svg";
 
 export {
   SpotifyLogo,
@@ -15,5 +15,5 @@ export {
   SearchIconActive,
   AddPlayListIcon,
   SettingsIcon,
-  PlayIcon,
+  PlayCardIcon,
 };
