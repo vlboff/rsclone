ab2f539fa9bea1c7e82af291776d65d1f53f6582
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2023-02-02 13:50:24 +0300

    refactor: rename styles

--- src/components/view/PlayingBar.tsx
+++ src/components/view/PlayingBar.tsx
@@ -14,7 +14,7 @@ function PlayingBar() {
   }
 
   return (
-    <footer className='playing-bar'>
+    <>
       <div className="cover-art_expanded">
         <a href="/">
           <div>
@@ -25,76 +25,78 @@ function PlayingBar() {
           <Hide />
         </button>
       </div>
-      <div className='playing-bar__wrapper'>
-        <div className='playing-widget'>
-          <div className='cover-art'>
-            <div className='cover-art__container'>
-              <a href="/">
-                <div>
-                  <img src="https://i.scdn.co/image/ab67706f00000002708c623e2e1df4607775381b" alt='cover art' />
-                </div>
-              </a>
-              <button className='show-button' onClick={expandCoverArt}>
-                <Show />
-              </button>
+      <footer className='playing-bar'>
+        <div className='playing-bar__wrapper'>
+          <div className='playing-widget'>
+            <div className='cover-art'>
+              <div className='cover-art__container'>
+                <a href="/">
+                  <div>
+                    <img src="https://i.scdn.co/image/ab67706f00000002708c623e2e1df4607775381b" alt='cover art' />
+                  </div>
+                </a>
+                <button className='show-button' onClick={expandCoverArt}>
+                  <Show />
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="track-info">
-            <div className='track-name'>
-              <a href="/">Lorem, ipsum dolor.</a>
+            <div className="track-info">
+              <div className='track-name'>
+                <a href="/">Lorem, ipsum dolor.</a>
+              </div>
+              <div className='track-author'>
+                <a href="/">Lorem, ipsum.</a>
+              </div>
             </div>
-            <div className='track-author'>
-              <a href="/">Lorem, ipsum.</a>
-            </div>
-          </div>
-          <button className='add-button player-tool-button'>
-            <Heart />
-          </button>
-        </div>
-        <div className='player-controls'>
-          <div className="player-controls__buttons">
-            <div className="player-controls__buttons-left">
-              <button className='player-tool-button'>
-                <Shuffle />
-              </button>
-              <button className='player-tool-button'>
-                <ButtonBack />
-              </button>
-            </div>
-            <button className='play-pause-button'>
-              <ButtonPlay />
+            <button className='add-button player-tool-button'>
+              <Heart />
             </button>
-            <div className="player-controls__buttons-right">
-              <button className='player-tool-button'>
-                <ButtonNext />
+          </div>
+          <div className='player-controls'>
+            <div className="player-controls__buttons">
+              <div className="player-controls__buttons-left">
+                <button className='player-tool-button'>
+                  <Shuffle />
+                </button>
+                <button className='player-tool-button'>
+                  <ButtonBack />
+                </button>
+              </div>
+              <button className='play-pause-button'>
+                <ButtonPlay />
               </button>
-              <button className='player-tool-button'>
-                <Repeat />
-              </button>
+              <div className="player-controls__buttons-right">
+                <button className='player-tool-button'>
+                  <ButtonNext />
+                </button>
+                <button className='player-tool-button'>
+                  <Repeat />
+                </button>
+              </div>
+            </div>
+            <div className="playback-bar">
+              <div className='playback-position'>1:45</div>
+              <input type="range" id="playback-range__input" />
+              <div className='playback-duration'>3:51</div>
             </div>
           </div>
-          <div className="playback-bar">
-            <div className='playback-position'>1:45</div>
-            <input type="range" id="playback-range__input" />
-            <div className='playback-duration'>3:51</div>
-          </div>
-        </div>
-        <div className='player-tools'>
-          <button className='player-tool-button'>
-            <Microphone />
-          </button>
-          <button className='player-tool-button'>
-            <Queue />
-          </button>
-          <div className="volume-bar">
+          <div className='player-tools'>
             <button className='player-tool-button'>
-              <VolumeOn />
+              <Microphone />
             </button>
-            <input type="range" id="volume-bar__range" />
+            <button className='player-tool-button'>
+              <Queue />
+            </button>
+            <div className="volume-bar">
+              <button className='player-tool-button'>
+                <VolumeOn />
+              </button>
+              <input type="range" id="volume-bar__range" />
+            </div>
           </div>
         </div>
-      </div>
-    </footer >
+      </footer >
+    </>
   )
 }
 
