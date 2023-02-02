eafc0b4edf8bcaabdb4ffb777b7ba427e0ecb95b
Uladzimir <vl.barbikov@gmail.com>
2023-02-02 18:45:41 +0300

    feat: add login

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MixesBlock from "../components/MixesBlock";
 import SettingsBar from "../components/SettingsBar";
+import axios from "axios";
+
+interface ICategories {
+  name: string;
+}
 
 function HomePage() {
-  const blockName = [
-    "Today's biggest hits",
-    "Featured Charts",
-    "Workout",
-    "Fresh new music",
-    "Mood",
-  ];
+  const [currentToken, setCurrentToken] = useState<string>("");
+  const [categories, setCategories] = useState<ICategories[]>([]);
+
+  const addCategories = async () => {
+    const { data } = await axios.get(
+      "https://api.spotify.com/v1/browse/categories",
+      {
+        headers: {
+          Authorization: `Bearer ${currentToken}`,
+        },
+      }
+    );
+    console.log(data.categories.items);
+    setCategories(data.categories.items);
+  };
+
+  useEffect(() => {
+    addCategories();
+  }, []);
+
+  // const blockName = [
+  //   "Today's biggest hits",
+  //   "Featured Charts",
+  //   "Workout",
+  //   "Fresh new music",
+  //   "Mood",
+  // ];
 
   return (
     <div className="home-page">
-      <SettingsBar />
+      <SettingsBar setCurrentToken={setCurrentToken} />
       <div className="mixes-block-wrapper">
-        {blockName.map((item) => (
-          <MixesBlock name={item} />
+        {categories.map((item) => (
+          <MixesBlock name={item.name} />
         ))}
       </div>
     </div>
