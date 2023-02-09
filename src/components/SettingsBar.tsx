11de0008599c24804a6b9974f1e77ad3d3eb4edc
George7836 <96310979+George7836@users.noreply.github.com>
2023-02-09 18:02:10 +0300

    feat/add goBack and goForward functionality

--- src/components/SettingsBar.tsx
+++ src/components/SettingsBar.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IconSettings, ArrowRightIcon, ArrowLeftIcon } from "../icons";
 import SearchBar from './SearchBar';
 import ButtonMenu from './ButtonMenu';
+import { useNavigate } from "react-router-dom";
 
 enum iconColor {
   white = "white",
@@ -10,19 +11,51 @@ enum iconColor {
 
 function SettingsBar() {
   const currentIconColor = iconColor.white;
+  const navigate = useNavigate()
+  const [disableLeft, setDisableLeft] = useState(true);
+  const [disableRight, setDisableRight] = useState(true);
+  const [counterR, setCounterR] = useState(0)
+  const [counterL, setCounterL] = useState(0)
+
+  const goBack = () => {
+    navigate(-1)
+    setCounterL(counterL - 2)
+    setCounterR(counterR + 1)
+  }
+  const goForward = () => {
+    navigate(1)
+    setCounterR(counterR - 1)
+  }
+
+  useEffect(() => {
+    setCounterL(counterL + 1)
+  }, [window.location.href])
+
+  useEffect(() => {
+
+    if(counterL > 1) {
+      setDisableLeft(false)
+    } else if(counterL <= 1) {
+      setDisableLeft(true)
+    } 
+    
+    if(counterR > 0) {
+      setDisableRight(false)
+    } else if(counterR <= 0) {
+      setDisableRight(true)
+    }
+
+  }, [counterL, counterR])
 
   return (
     <div className="settings-bar">
       <div className='settings-bar__block-left'>
         <div className="settings-bar__arrows">
-          <button className='arrow btn-reset'><ArrowLeftIcon className='arrow--left'/></button>
-          <button className='arrow btn-reset'><ArrowRightIcon className='arrow--right'/></button>
+          <button className='arrow btn-reset' disabled={disableLeft} onClick={() => goBack()}><ArrowLeftIcon className='arrow--left'/></button>
+          <button className='arrow btn-reset' disabled={disableRight} onClick={() => goForward()}><ArrowRightIcon className='arrow--right'/></button>
         </div>
       </div>
       <ButtonMenu/>
-      {/* <div className="settings-bar-icon">
-        <IconSettings fill={currentIconColor} />
-      </div> */}
     </div>
   );
 }
