235fe4863fa1673cbab3d4d1a7a83c7869763f86
Artyom Pankov <apankov4696@gmail.com>
2023-02-17 20:59:59 +0300

    fix: add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import PlayingBar from "./components/view/PlayingBar";
 import AuthPage from "./pages/AuthPage";
